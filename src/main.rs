df2d4fe1529c0362506475f2acdcc35c561a2a0f
Vanderlei Jordao de Almeida Junior <vanderlei.jordao.jr@outlook.com>
2021-06-23 21:53:24 -0300

    Extracting run function and adding error handling

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
     let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
-    let contents =
-        fs::read_to_string(config.filename).expect("Something went wrong reading the file.");
-    println!("Text: {}", contents);
+
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    Ok(())
 }
 
 struct Config {
@@ -21,10 +32,12 @@ struct Config {
 impl Config {
     fn new(args: &[String]) -> Result<Config, &str> {
         if args.len() < 3 {
-            return Err("not enought arguments.");
+            return Err("not enough arguments.");
         }
+
         let query = args[1].clone();
         let filename = args[2].clone();
+
         Ok(Config { query, filename })
     }
 }
