39d55097e8e8d2a61cb91a706df886367120c1f3
Vanderlei Jordao de Almeida Junior <vanderlei.jordao.jr@outlook.com>
2021-06-23 21:36:29 -0300

    Restructuring args into config
    Signed-off-by: Vanderlei Jordao de Almeida Junior <vanderlei.jordao.jr@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,8 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
-    let contents =
-        fs::read_to_string(config.filename).expect("Something went wrong reading the file.");
+    let config = Config::new(&args);
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file.");
     println!("Text: {}", contents);
 }
 
@@ -14,8 +13,10 @@ struct Config {
     filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-    Config { query, filename }
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Config { query, filename }
+    }
 }
