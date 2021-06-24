483bec80736fe073bc8bbe99e9742a5223b3ae08
Vanderlei Jordao de Almeida Junior <vanderlei.jordao.jr@outlook.com>
2021-06-23 21:59:05 -0300

    Refactoring lib and main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use rgrep::run;
+use rgrep::Config;
+
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
 
 fn main() {
@@ -15,29 +16,5 @@ fn main() {
         println!("Application error: {}", e);
 
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments.");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
     }
 }
